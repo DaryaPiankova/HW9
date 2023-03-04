00a64529ccea0bfae1ec8bc454ee1886432edf6b
Daria <darya.piankova@yandex.ru>
2023-03-04 10:54:13 +0500

    init commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int  OutputNumbers(int m, int n)
+{
+    if (m== n)
+        return m;
+    return  OutputNumbers(m, n-1)+n;
+}
+
+
+Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(OutputNumbers(m,n));
